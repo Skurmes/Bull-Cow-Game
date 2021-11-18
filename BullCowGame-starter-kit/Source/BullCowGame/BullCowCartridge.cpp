afaf415f6fbdbc59d460d0289405305959ce0138
Tristan Wood <triawoo@email.ecok.edu>
2021-11-17 20:11:25 -0600

    Moved FString HiddenWord to private variable.

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -8,18 +8,18 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
 
     Super::BeginPlay();
+    
     PrintLine(TEXT("Welcome to the Bull Cow Game!"));
     PrintLine(TEXT("Guess the 5 letter word.")); //Magic number Remove!
     PrintLine(TEXT("\nPress enter to continue."));
-
+    HiddenWord = TEXT("magic");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
 
     ClearScreen();
-    FString HiddenWord = TEXT("magic"); //Move outside of this function.
-    
+  
     if (Input == HiddenWord)
     {
         PrintLine(TEXT("You Win!")); 
